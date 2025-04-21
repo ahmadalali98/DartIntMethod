2d3061a44ee1d601f14ca4b1cc78d5dd2142a8ec
ahmadalali98 <ahmadalali.kh@gmail.com>
2025-04-21 14:59:23 +0300

    update

--- mian.dart
+++ mian.dart
@@ -1,4 +1,12 @@
 void main() {
-  String name = 'ahmad123';
-  print(name);
+  String name = 'ahmad';
+  int age = 26;
+  double myhighest = 17.4;
+
+  print(name[0].toUpperCase());
+
+  print(myhighest.ceil());
+  print(myhighest.floor());
+  print(myhighest.round());
+  print(myhighest.isNegative);
 }
