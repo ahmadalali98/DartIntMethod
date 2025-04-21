549b1558330e557af1fcba23f495488bfdb75a45
ahmadalali98 <ahmadalali.kh@gmail.com>
2025-04-21 15:33:46 +0300

    update

--- mian.dart
+++ mian.dart
@@ -2,6 +2,7 @@ void main() {
   String name = 'ahmad';
   int age = 26;
   double myhighest = 17.4;
+  int myyears = 1998;
 
   print(name[0].toUpperCase());
 
@@ -9,4 +10,7 @@ void main() {
   print(myhighest.floor());
   print(myhighest.round());
   print(myhighest.isNegative);
+
+  int myagesAfter10Year = age + 10;
+  print("my age After 10 yaers $myagesAfter10Year");
 }
